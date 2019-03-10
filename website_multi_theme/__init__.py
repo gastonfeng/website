5a7ee074e3fde071d5ef5b07e24eaf4130d45964
gastonfeng <gastonfeng@gmail.com>
2019-03-10 17:54:16 +0800

    test project cost

--- website_multi_theme/__init__.py
+++ website_multi_theme/__init__.py
@@ -1,6 +1,6 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from . import controllers
 from . import models
 from . import wizards
+from . import controllers
 from .hooks import uninstall_hook
